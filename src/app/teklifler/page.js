454151e5bdc46c8d966459ccb79e1b00bfafcec6
Yasemin <ysmnckr06@icloud.com>
2026-04-04 23:33:27 +0300

    ilk versiyon

---
+++ src/app/teklifler/page.js
@@ -0,0 +1,7 @@
+export default function TekliflerPage() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-slate-100">
+      <h1 className="text-2xl font-bold">Teklifler Sayfası</h1>
+    </main>
+  );
+}
