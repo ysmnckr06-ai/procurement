f97b2712e7f779b8c34c4a2a706ea3e8714803e4
Yasemin <yasemin@gmail.com>
2026-05-09 20:01:48 +0300

    secure cors

--- backend/main.py
+++ backend/main.py
@@ -45,8 +45,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "http://127.0.0.1:3000",
-        "http://192.168.1.5:3000",
+        "https://procurement-dun.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
