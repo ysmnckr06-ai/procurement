c44d83864839b138388f322330925a2b72b9a824
Yasemin <yasemin@gmail.com>
2026-05-10 19:46:46 +0300

    Allow local frontend cors

--- main.py
+++ main.py
@@ -47,6 +47,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        "http://127.0.0.1:3000",
         "http://localhost:3000",
         "https://procurement-dun.vercel.app",
     ],
