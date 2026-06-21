4a5a0f8afeda4252d2ba6b0beba31342065b6e41
Yasemin <ysmnckr06@icloud.com>
2026-06-21 20:45:41 +0300

    Remove legacy middleware entry

--- procurement-platform/middleware.js
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-export function middleware(_request) {
-  return NextResponse.next();
-}
-
-export const config = {
-  matcher: ["/dashboard/:path*", "/login", "/register"],
-};
